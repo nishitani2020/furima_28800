d71d96b28cdb46039a5e870d7f05b83614811095
nishitani2020 <69030775+nishitani2020@users.noreply.github.com>
2020-08-27 15:52:10 +0900

    新規登録機能の実装

--- README.md
+++ README.md
@@ -29,12 +29,12 @@ Things you may want to cover:
 
 | Column           | Type   | Options     |
 | ---------------- | ------ | ----------- |
-| name             | string | null: false |
+| nickname         | string | null: false |
 | email            | string | null: false |
-| family_name      | string | null: false |
-| family_name_kana | string | null: false |
 | first_name       | string | null: false |
 | first_name_kana  | string | null: false |
+| last_name        | string | null: false |
+| last_name_kana   | string | null: false |
 | birth_date       | date   | null: false |
 
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 
 
   private
@@ -18,4 +10,9 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :first_name_kana, :last_name, :last_name_kana, :birth_date])
+  end
+
 end
