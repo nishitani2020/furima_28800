065c6453bfea929b3f3fdd1c037ec2ebc5d65466
nishitani2020 <69030775+nishitani2020@users.noreply.github.com>
2020-08-27 19:15:19 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :password, presence: true, length: { minimum: 6 } , format: { with: /\A[a-zA-Z]+\z/ } ,confirmation: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ン]+\z/ }
+  validates :last_name_kana, presence: true,format: { with: /\A[ァ-ン]+\z/ }
+  validates :birth_date, presence: true
+
 end
