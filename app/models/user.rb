ae3ea2015eb36568ed85b55540c5db61cbe49ee0
nishitani2020 <69030775+nishitani2020@users.noreply.github.com>
2020-08-28 20:26:02 +0900

    Rubocopでの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-  validates :password, length: { minimum: 6 } , format: { with: /\A[0-9A-Za-z]+\z/ }
+  validates :password, length: { minimum: 6 }, format: { with: /\A[0-9A-Za-z]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ン]+\z/ }
-  validates :last_name_kana, presence: true,format: { with: /\A[ァ-ン]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
   validates :birth_date, presence: true
-
 end
