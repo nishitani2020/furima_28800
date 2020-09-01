58158dc862ab1bf97d42a0ad8d761607f9d606ed
nishitani2020 <69030775+nishitani2020@users.noreply.github.com>
2020-09-01 16:27:10 +0900

    itemモデルのテスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :image, :name, :explanation, :price, presence: true
-  validates :price, inclusion: {in: 300..9999999}
+  validates :price, inclusion: {in: 300..9999999}, format: { with: /\A[0-9]+\z/ }
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name { 'name' }
+    explanation { 'ああああ' }
+    category_id { '2' }
+    status_id { '2' }
+    delivery_cost_id { '2' }
+    delivery_area_id { '2' }
+    delivery_day_id { '2' }
+    price { '500' }
+    association :user
   end
 end
