6de3aea17004154b41ebefcb32982c552439095e
nishitani2020 <69030775+nishitani2020@users.noreply.github.com>
2020-09-03 12:49:01 +0900

    Rubocopでの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :find_item, only: :order
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
