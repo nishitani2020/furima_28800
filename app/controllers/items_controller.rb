a6249fd3b1bb09063bb446b8825ca8847426e832
nishitani2020 <69030775+nishitani2020@users.noreply.github.com>
2020-09-04 12:31:33 +0900

    before_action修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :find_item, only: :order
+  before_action :find_item, only: [:order, :show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
       redirect_to root_path
